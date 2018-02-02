16cc3950254a5747ffbb23649971ae60d575a4cf
wdimamiller <wdimamiller@gmail.com>
2018-02-02 17:29:53 +0200

    Fix grid data model

--- src/main/java/org/angelholm/composer/ListPatientComposer.java
+++ src/main/java/org/angelholm/composer/ListPatientComposer.java
@@ -1,19 +1,22 @@
 package org.angelholm.composer;
 
+import org.angelholm.model.User;
 import org.angelholm.service.PatientService;
 import org.hl7.fhir.dstu3.model.Patient;
 import org.zkoss.bind.annotation.AfterCompose;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.select.SelectorComposer;
-import org.zkoss.zul.Grid;
-import org.zkoss.zul.ListModelArray;
-import org.zkoss.zul.Row;
-import org.zkoss.zul.RowRenderer;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.*;
 
 import java.util.ArrayList;
 
 public class ListPatientComposer extends SelectorComposer {
 
+    @Wire
     Grid gridPatients;
     ArrayList<Patient> listPatients;
 
@@ -21,46 +24,47 @@ public class ListPatientComposer extends SelectorComposer {
     public void doAfterCompose(Component comp) throws Exception {
         super.doAfterCompose(comp);
 
+        //fill grid
         PatientService patientService = new PatientService();
-        this.listPatients = patientService.getListPatiens();
+        listPatients = patientService.getListPatiens();
 
+        ListModelList<Patient> gridListModel = new ListModelList<>(listPatients);
 
-        ListModelArray<Patient> gridListModel = new ListModelArray<>(listPatients);
+        gridPatients.setModel(gridListModel);
 
-        System.out.println(gridListModel.toString());
+        gridPatients.setRowRenderer((RowRenderer) (row, data, index) -> {
+            final Patient patient = (Patient) data;
 
-        gridPatients = new Grid();
-        if (gridListModel.getSize() > 0) {
-            gridPatients.setModel(gridListModel);
+            row.appendChild(new Label(patient.getName().get(0).getFamily()));
+            row.appendChild(new Label(patient.getName().get(0).getGiven().get(0).toString()));
+            row.appendChild(new Label("qqqq"));
+            row.appendChild(new Label("qqqq"));
+            row.appendChild(new Label("qqqq"));
 
-        }
-        gridPatients.setRowRenderer(new RowRenderer() {
-            @Override
-            public void render(Row row, Object data, int index) throws Exception {
-                final Patient patient = (Patient) data;
-               // initOperation(prod).setParent(row);
-
-
-
-            }
-
-/*
-            public void render(Row row, Object data) throws Exception {
-                final Product prod = (Product) data;
-                 
-                Image img = new Image(prod.getImgPath());
-                img.setWidth("70px");
-                img.setHeight("70px");
-                img.setParent(row);
-                new Label(prod.getName()).setParent(row);
-                new Label("" + prod.getPrice()).setParent(row);
-                new Label("" + prod.getQuantity()).setParent(row);
-                new Label(YYYY_MM_DD_hh_ss.format(prod.getCreateDate())).setParent(row);
-                initOperation(prod).setParent(row);
-            }
-
-*/
         });
+
+        //add event
+        for (Component row : gridPatients.getRows().getChildren()) {
+            row.addEventListener(Events.ON_CLICK, new EventListener<Event>() {
+
+                @Override
+                public void onEvent(Event arg0) throws Exception {
+                    Row row = (Row) arg0.getTarget();
+                    Boolean rowSelected = (Boolean) row.getAttribute("Selected");
+
+                    if (rowSelected != null && rowSelected) {
+                        row.setAttribute("Selected", false);
+                        // row.setStyle("");
+                        row.setSclass("");
+                    } else {
+                        row.setAttribute("Selected", true);
+                        // row.setStyle("background-color: #CCCCCC !important");   // inline style
+                        row.setSclass("z-row-background-color-on-select");         // sclass
+                    }
+                    
+                }
+            });
+        }
 
 
     }
