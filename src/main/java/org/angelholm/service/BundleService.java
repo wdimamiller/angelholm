f95e872207affee6e92ebaaa541f792109bce2b5
wdimamiller <wdimamiller@gmail.com>
2018-03-16 17:06:21 +0200

    ValueSet internationalization

---
+++ src/main/java/org/angelholm/service/BundleService.java
@@ -0,0 +1,17 @@
+package org.angelholm.service;
+
+import ca.uhn.fhir.model.primitive.StringDt;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.gclient.ICriterion;
+import ca.uhn.fhir.rest.gclient.StringClientParam;
+import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.Patient;
+import org.hl7.fhir.dstu3.model.Resource;
+import org.hl7.fhir.dstu3.model.Type;
+
+public class BundleService {
+
+    private IGenericClient client =  FhirGenericClientService.getInstance();
+
+
+}
