faa85394ca1a689c6bcb3f2b130a7a1c2713dfa7
wdimamiller <wdimamiller@gmail.com>
2018-03-26 13:19:38 +0300

    add Security Handler

--- src/main/java/org/angelholm/model/UserPrincipal.java
+++ src/main/java/org/angelholm/model/UserPrincipal.java
@@ -4,7 +4,6 @@ import org.hl7.fhir.dstu3.model.Practitioner;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
-
 import java.util.Collection;
 
 
@@ -12,7 +11,13 @@ public class UserPrincipal extends User{
 
     private Practitioner practitioner;
 
-    public UserPrincipal( Practitioner practitioner, String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities){
+    public UserPrincipal( Practitioner practitioner,
+                          String username,
+                          String password,
+                          boolean enabled,
+                          boolean accountNonExpired,
+                          boolean credentialsNonExpired,
+                          boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities){
         super( username,  password,  enabled,  accountNonExpired,  credentialsNonExpired,  accountNonLocked,  authorities);
         this.practitioner = practitioner;
     }
